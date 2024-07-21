63ad2abce2cb10d151bd211836c686f15d1b6ba5
maihoonhariom <rajputhariom835@gmail.com>
2024-07-21 23:12:08 +0530

    update

--- Patterns/triangle.java
+++ Patterns/triangle.java
@@ -1,3 +1,11 @@
+/*
+                *
+                **
+                ***
+                ****
+                *****
+ */
+
 public class triangle {
     public static void main(String[] args) {
         int n = 5;
