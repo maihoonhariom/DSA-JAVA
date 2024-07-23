61c93445afb157d8106681ed1599f8f24b3a6b99
maihoonhariom <rajputhariom835@gmail.com>
2024-07-23 15:24:34 +0530

    PALINDROMIC Pattern with Numbers pattern

---
+++ Patterns/palDromNum.java
@@ -0,0 +1,36 @@
+/*
+            1
+           212
+          32123
+         4321234
+        543212345
+ */
+
+
+public class palDromNum {
+    public static void main(String args[]){
+        int n =5;
+        int count=n;
+        for(int i=n; i>0; i--){
+            count=n-i+1;
+            for(int j=1; j<=n*2-i; j++){
+                if(j>=i){
+                    
+                    if(j<=n){
+                        System.out.print(count--);
+                        if (j==n) {
+                            count=1;
+                        }
+                       
+                    }
+                    else{                        
+                        System.out.print(++count);
+                    }
+                }
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
